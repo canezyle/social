1aa5bb9d0ba5598e1a543326648a3abb156bf65d
LAPTOP-RJ27I3MN\User <czgaminghd04@gmail.com>
2021-07-25 01:23:41 +0800

    initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <v-btn
+            class="mx-2"
+            fab
+            dark
+            small
+            color="primary"
+        >
+            <v-icon dark>
+                mdi-minus
+            </v-icon>
+        </v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
